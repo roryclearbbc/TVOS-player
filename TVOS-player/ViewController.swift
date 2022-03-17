96338f374454293d2cf167573a13755523539e25
Carkzis <marc.jowett@gmail.com>
2022-03-17 14:33:37 +0000

    Add 4 types of media to play in UI

--- TVOS-player/ViewController.swift
+++ TVOS-player/ViewController.swift
@@ -13,41 +13,36 @@ import SMP
 class ViewController: UIViewController {
 
     @IBAction func playVideo(_ sender: Any) {
-        
         let vod =  "https://vod-hls-uk.live.cf.md.bbci.co.uk/usp/auth/vod/piff_abr_full_hd/efd8aa-m000crsj/vf_m000crsj_d8ecfb25-9648-4ca8-8b19-664f28c3344a.ism/vf_m000crsj_d8ecfb25-9648-4ca8-8b19-664f28c3344a-audio_eng=48000-video=281000.m3u8"
-        
-        _ = "https://vs-hls-push-uk-live.akamaized.net/x=3/i=urn:bbc:pips:service:bbc_one_hd/mobile_wifi_main_sd_abr_v2.m3u8"
-        
-        _ = "https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/high/ak/bbc_6music.m3u8"
-        
-        _ =  "https://aod-hls-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_audio/9b54cc-p0bkp2ln/vf_p0bkp2ln_e2bf87d2-bd01-403c-ae58-c504841775ad.ism/mobile_wifi_main_sd_abr_v2_uk_hls_master.m3u8?__gda__=1647467687_05a68a9eab254be31c203c157412f575"
-        
-       // let playerItemProvider = MediaSelectorItemProviderBuilder(VPID: "m000ryh8", mediaSet: "mobile-phone-main", AVType: .video, streamType: .VOD, avStatisticsConsumer: MyAvStatisticsConsumer())
+        play(vod)
+    }
+    
+    @IBAction func playAudio(_ sender: Any) {
+        let aod =  "https://aod-hls-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_audio/e7881a-m0013jmz/vf_m0013jmz_b165ea45-5c43-4a45-820b-deaf10d5abff.ism/mobile_wifi_main_sd_abr_v2_uk_hls_master.m3u8?__gda__=1647548921_06e57756b5cec30015a8e0bd2321987d"
+        play(aod)
+    }
+    
+    @IBAction func playVideoSimulcast(_ sender: Any) {
+        let videoSimulcast = "https://vs-hls-push-uk-live.akamaized.net/x=3/i=urn:bbc:pips:service:bbc_one_hd/mobile_wifi_main_sd_abr_v2.m3u8"
+        play(videoSimulcast)
+    }
+    
+    @IBAction func playAudioSimulcast(_ sender: Any) {
+        let audioSimulcast = "https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/high/aks/bbc_6music.m3u8"
+        play(audioSimulcast)
+    }
+    
+    func play(_ mediaUrl: String) {
         let statisticsConsumer = StubBBCSMPAVStatisticsConsumer()
-        let playerItemProvider = BBCSMPStaticURLPlayerItemProvider(url: URL(string: vod)!, avStatisticsConsumer: statisticsConsumer)
+        let playerItemProvider = BBCSMPStaticURLPlayerItemProvider(url: URL(string: mediaUrl)!, avStatisticsConsumer: statisticsConsumer)
         
         let player = BBCSMPPlayerBuilder().withPlayerItemProvider(playerItemProvider).build()
         let playerViewController = player.buildUserInterface().buildViewController()
 
         let navigationController: UINavigationController = UINavigationController(rootViewController: playerViewController)
         self.present(navigationController, animated: true, completion: nil)
-        /*
-        
-        guard let url = URL(string: videosim) else { return }
-
-        // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-        let player = AVPlayer(url: url)
-
-        // Create a new AVPlayerViewController and pass it a reference to the player.
-        let controller = AVPlayerViewController()
-        controller.player = player
-
-        // Modally present the player and call the player's play() method when complete.
-        present(controller, animated: true) {
-            player.play()
-        }
-         */
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
