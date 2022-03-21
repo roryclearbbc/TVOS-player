60c62adc94c282a12352af56165fe65ee46f12ca
Carkzis <marc.jowett@gmail.com>
2022-03-21 11:26:50 +0000

    Change url to vpid

--- TVOS-player/ViewController.swift
+++ TVOS-player/ViewController.swift
@@ -13,42 +13,36 @@ import SMP
 class ViewController: UIViewController {
 
     @IBAction func playVideo(_ sender: Any) {
-        let vod =  "https://vod-hls-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_hd/efd8aa-m000crsj/vf_m000crsj_d8ecfb25-9648-4ca8-8b19-664f28c3344a.ism/mobile_wifi_main_sd_abr_v2_hls_master.m3u8?__gda__=1647874667_781c7d692f0878f597bf248d0c5b8462"
-        let subtitles =
-            "https://vod-sub-uk-live.akamaized.net/iplayer/subtitles/ng/modav/bUnknown-7c4985de-20fb-451d-9069-0c46e28d6f2c_m000crsj_pips-pid-m000crsj_1638872506838.xml?__gda__=1647874667_82bdbfa6364f87a7ae2344410920aad5"
-        play(vod, subtitles)
+        let vpid = "m000crsj"
+        play(vpid, streamType: .VOD, avType: .video)
     }
     
     @IBAction func playAudio(_ sender: Any) {
-        let aod =  "https://aod-hls-uk-live.akamaized.net/usp/auth/vod/piff_abr_full_audio/77dc2a-m00153sq/vf_m00153sq_643b08fe-e4a8-42ed-8cff-d0d761c3a592.ism/mobile_wifi_main_sd_abr_v2_uk_hls_master.m3u8?__gda__=1647877256_3fea8e68a81fd1b62c0f72cf610aa32e"
-        play(aod)
+        let vpid = "m00153sq"
+        play(vpid, streamType: .VOD, avType: .audio)
     }
     
     @IBAction func playVideoSimulcast(_ sender: Any) {
-        let videoSimulcast = "https://vs-hls-push-uk-live.akamaized.net/x=3/i=urn:bbc:pips:service:bbc_one_hd/mobile_wifi_main_sd_abr_v2.m3u8"
-        play(videoSimulcast, isLive: true)
+        let vpid = "bbc_one_hd"
+        play(vpid, streamType: .simulcast, avType: .video)
     }
     
     @IBAction func playAudioSimulcast(_ sender: Any) {
-        let audioSimulcast = "https://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/uk/high/aks/bbc_6music.m3u8"
-        play(audioSimulcast, isLive: true)
+        let vpid = "bbc_6music"
+        play(vpid, streamType: .simulcast, avType: .video)
     }
     
-    func play(_ mediaUrl: String, _ subtitlesUrl: String = "", isLive: Bool = false) {
+    func play(_ mediaUrl: String, streamType: BBCSMPStreamType, avType: BBCSMPAVType) {
         let statisticsConsumer = StubBBCSMPAVStatisticsConsumer()
-        let playerItemProvider = !subtitlesUrl.isEmpty ?
-            BBCSMPStaticURLPlayerItemProvider(url: URL(string: mediaUrl)!, subtitleUrl: URL(string: subtitlesUrl)!, avStatisticsConsumer: statisticsConsumer) :
-            BBCSMPStaticURLPlayerItemProvider(url: URL(string: mediaUrl)!, avStatisticsConsumer: statisticsConsumer)
-        if (isLive) {
-            playerItemProvider.streamType = .simulcast
-        }
+        
+        // mediaSet is "mobile-phone-main" but it's tvOS?
+        let itemProvider = MediaSelectorItemProviderBuilder(VPID: mediaUrl, mediaSet: "mobile-phone-main", AVType: .video, streamType: streamType, avStatisticsConsumer: statisticsConsumer).buildItemProvider()
         
         let decoderFactory = BBCSMPAVDecoderFactory()
         
         let player = BBCSMPPlayerBuilder()
-            .withPlayerItemProvider(playerItemProvider)
+            .withPlayerItemProvider(itemProvider)
             .withSubtitlesDefaultedOn()
-            // MARK: THIS IS IMPORTANT!
             .withDecoderFactory(decoderFactory)
             .build()
         
